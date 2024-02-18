7519478bfdafb0bc8b360c4c25075751a7d11840
Kacper <kacpersikora.praca@gmail.com>
2024-02-18 18:57:37 +0100

    read more update

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const footerYear = document.querySelector(".footer__year");
 const allNavItems = document.querySelectorAll(".nav__link-mobile");
 const body = document.querySelector("body");
 
+
 const handleNavBtn = () => 
 {
     navBtn.classList.toggle("is-active");
@@ -26,6 +27,29 @@ const handleCurrentYear = () =>
     footerYear.innerText = year;
 }
 
-handleCurrentYear();
 
+
+handleCurrentYear();
 navBtn.addEventListener("click", handleNavBtn);
+
+const readMoreBtn = document.querySelectorAll(".info__box-link");
+const readMoreText = document.querySelector(".info__box-text-read-more");
+const readMoreBox = document.querySelectorAll(".info__box");
+
+readMoreBox.forEach(box =>
+{
+
+    box.addEventListener("click", e => 
+    {
+        const current = e.target;
+        if (!current.className.includes("info__box-link"))
+        {
+            return;
+        }
+
+        const currentText = e.target.parentNode.querySelector(".info__box-text-read-more");
+        currentText.classList.toggle("info__box-text-read-more--open");
+        current.textContent = current.textContent.includes("read more...") ? "read less..." : "read more..."
+    });
+}
+)
